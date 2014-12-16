3df20309999e5ac5f75db42112fe03ff1ea5e1e3
rasmussenj <jacob.rasmussen@students.fhnw.ch>
2014-12-16 08:54:49 +0100

    optimizing code
    optimizing the code, because of performance problems

--- paint.py
+++ paint.py
@@ -6,7 +6,6 @@ class MakeCurves():
         #Variable
         self.dataSet = []
 
-        self.xMin = 0
         self.xMax = 0
 
         self.colorIndex = 0
@@ -20,7 +19,6 @@ class MakeCurves():
         self.color = color
         self.zoomX = zoomX
         self.yMinMax = yMinMax
-        self.xMin = 0
         self.xMax = len(dataSet) - 1
         self.repaint()
 
@@ -34,24 +32,23 @@ class MakeCurves():
         self.plotLines()
 
     def drawGrid(self):
-        self.grid = self.canvas.create_line(0, self.height/2, self.width, self.height/2, width=0, fill=self.gridColor)
+        self.grid = self.canvas.create_line(0, 0, self.width, 0, width=0, fill=self.gridColor)
 
     def plotLines(self):
 
         self.coordinates = []
-        for i in range(0, len(self.dataSet)):
-            x = float(i) / self.xMax * self.width * self.zoomX
+        for i in range(0, len(self.xMax+1)):
+            x = i / self.xMax * self.width * self.zoomX
             if self.dataSet[i] == "":
-                y = float(self.height)+1
-            elif float(self.dataSet[i]) > float(self.yMinMax[0]) or float(self.dataSet[i]) < float(self.yMinMax[1]):
-                y = self.height - float(self.dataSet[i])
+                y = self.height+1
             else:
-                y = float(self.height)+1
+                y = (self.dataSet[i]-self.yMinMax[0]) / (self.yMinMax[1]-self.yMinMax[0]) * self.height
+                y = self.height - y
             self.coordinates.append(x)
             self.coordinates.append(y)
 
         self.canvas.create_line(self.coordinates, fill=self.color)
-        self.canvas.coords(self.grid, 0, self.height/2, x, self.height/2)
+        self.canvas.coords(self.grid, 0, 0, x, 0)
         self.canvas.config(scrollregion=self.canvas.bbox(self.grid))
 
     # Event Handlers
